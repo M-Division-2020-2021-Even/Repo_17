332295a8fd10af1e152debab60f501651dce8c85
AnupKotur0305 <83597264+AnupKotur0305@users.noreply.github.com>
2021-06-28 19:04:29 +0530

    Add files via upload

---
+++ M DIV.c
@@ -0,0 +1,6 @@
+|---|---|---|---|
+|SL.NO| NAME                  | USN           | ROLL.NO  |
+| 1.  | ANUP A KOTUR          | 01FE20BEC100  |   1360   |
+| 2.  | S.CHANDU              | 01FE20BEE096  |   1362   |
+| 3.  | TEJAS MARIYAPPAGOUDER | 01FE20BEC226  |   1353   |
+| 4.  | ROHIT REDDY           | 01FE20BCS233  |   1321   |
